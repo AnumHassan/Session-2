a858600b3339f4e2703a0b6aa1be8f068b1f9b5d
AnumHassan <50082188+AnumHassan@users.noreply.github.com>
2019-11-10 17:13:00 +0500

    Add files via upload

---
+++ Bio.py
@@ -0,0 +1,17 @@
+def Bio(a,b,c):
+    x=input("Enter your name")
+    y=int(input("Enter your age"))
+    z=input(" Enter your Roll No")
+    a=x
+    b=y
+    c=z
+    
+def age(a):
+    if a >= 18:
+        print("She is an adult")
+    
+def P_1(a,b,c):
+    print(a,b,c)
+   
+
+    
